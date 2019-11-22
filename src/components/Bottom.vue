dd7759c36cf85aef86177e7293e42dde20820837
Tochsaporn Petchpol <bell_2530@hotmail.com>
2019-11-22 07:22:54 +0700

    Adding files.

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
     <b-navbar toggleable="lg" type="light" style="background-color: #e3f2fd;" fixed="bottom">
-        <b-navbar-brand class="mx-auto" >Copyright © 2019 Bell SCG Candidate / <font-awesome-icon icon="phone-alt" /> 084-651-4454 / <font-awesome-icon icon="envelope" /> Bell_2530@hotmail.com</b-navbar-brand>
+        <p class="mx-auto footer-text" >Copyright © 2019 Bell SCG Candidate / <font-awesome-icon icon="phone-alt" /> 084-651-4454 / <font-awesome-icon icon="envelope" /> Bell_2530@hotmail.com</p>
     </b-navbar>
   </div>
 </template>
